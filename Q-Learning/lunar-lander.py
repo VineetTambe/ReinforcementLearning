99094f573b6ade21c7754c9fcba19ce2ca4e98b3
Vineet Tambe <tambevineet@gmail.com>
2023-06-07 00:11:16 -0400

    Autoformatted

--- Q-Learning/lunar-lander.py
+++ Q-Learning/lunar-lander.py
@@ -2,14 +2,14 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-import time 
+import time
 
 env = gym.make("LunarLander-v2")
 observation, info = env.reset(seed=0)
 
 # print (len(observation))
 
-LEARNING_RATE = 0.1 
+LEARNING_RATE = 0.1
 DISCOUNT = 0.9
 EPISODES = 25000
 SHOW_EVERY = 2000
@@ -17,28 +17,32 @@ SHOW_EVERY = 2000
 DISCRETIZATION = 12
 
 DISCRETE_OBSERVATION_SPACE_SIZE = [DISCRETIZATION] * len(observation[:-2])
-discrete_os_win_size = (env.observation_space.high[:-2] - env.observation_space.low[:-2]) / DISCRETE_OBSERVATION_SPACE_SIZE   
+discrete_os_win_size = (
+    env.observation_space.high[:-2] - env.observation_space.low[:-2]
+) / DISCRETE_OBSERVATION_SPACE_SIZE
 
 epsilon = 0.5
 START_EPSILON_DECAYING = 1
 END_EPSILON_DECAYING = EPISODES // 2
 epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
-# print(DISCRETE_OBSERVATION_SPACE_SIZE)
-# print(discrete_os_win_size)
-# print(DISCRETE_OBSERVATION_SPACE_SIZE + [env.action_space.n])
-q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OBSERVATION_SPACE_SIZE + [env.action_space.n]))
+q_table = np.random.uniform(
+    low=-2, high=0, size=(DISCRETE_OBSERVATION_SPACE_SIZE + [env.action_space.n])
+)
 
 print("q_table.shape", q_table.shape)
 
 ep_rewqards = []
-aggre_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max': []}
+aggre_ep_rewards = {"ep": [], "avg": [], "min": [], "max": []}
 
 
 def get_discrete_state(state):
-    descrete_state = ((state[:-2] - env.observation_space.low[:-2]) / discrete_os_win_size)
-    descrete_state =  np.clip(descrete_state, 0 , DISCRETIZATION-1)
+    descrete_state = (
+        state[:-2] - env.observation_space.low[:-2]
+    ) / discrete_os_win_size
+    descrete_state = np.clip(descrete_state, 0, DISCRETIZATION - 1)
     return tuple(descrete_state.astype(int))
+
 
 for episode in tqdm(range(EPISODES)):
     episode_reward = 0
@@ -47,7 +51,6 @@ for episode in tqdm(range(EPISODES)):
         render = True
     else:
         render = False
-
 
     obs, info = env.reset()
 
@@ -66,32 +69,32 @@ for episode in tqdm(range(EPISODES)):
             action = np.random.randint(0, env.action_space.n)
 
         observation, reward, terminated, truncated, _ = env.step(action)
-        
+
         episode_reward += reward
-        
 
         # print(observation)
 
         new_discrete_state = get_discrete_state(observation)
 
         if not (terminated or truncated):
-
             # print(new_discrete_state)
 
             max_future_q = np.max(q_table[new_discrete_state])
-            current_q = q_table[discrete_state + (action, )]
-            
+            current_q = q_table[discrete_state + (action,)]
+
             # exponential decay
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (
+                reward + DISCOUNT * max_future_q
+            )
 
-            q_table[discrete_state + (action, )] = new_q
+            q_table[discrete_state + (action,)] = new_q
 
-        elif (observation[0],observation[2]) == (0,0):
+        elif (observation[0], observation[2]) == (0, 0):
             print(f"Landed successfully on episode {episode}!")
-            q_table[discrete_state + (action, )] = 0
-        
+            q_table[discrete_state + (action,)] = 0
+
         discrete_state = new_discrete_state
-    
+
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
         # print(reward)
@@ -101,12 +104,14 @@ for episode in tqdm(range(EPISODES)):
 
     if not episode % SHOW_EVERY:
         average_reward = sum(ep_rewqards[-SHOW_EVERY:]) / len(ep_rewqards[-SHOW_EVERY:])
-        aggre_ep_rewards['ep'].append(episode)
-        aggre_ep_rewards['avg'].append(average_reward)
-        aggre_ep_rewards['min'].append(min(ep_rewqards[-SHOW_EVERY:]))
-        aggre_ep_rewards['max'].append(max(ep_rewqards[-SHOW_EVERY:]))
-        print(f"Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}")
-    
+        aggre_ep_rewards["ep"].append(episode)
+        aggre_ep_rewards["avg"].append(average_reward)
+        aggre_ep_rewards["min"].append(min(ep_rewqards[-SHOW_EVERY:]))
+        aggre_ep_rewards["max"].append(max(ep_rewqards[-SHOW_EVERY:]))
+        print(
+            f"Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}"
+        )
+
 env.close()
 # plt.plot(aggre_ep_rewards['ep'], aggre_ep_rewards['avg'], label="avg")
 # plt.plot(aggre_ep_rewards['ep'], aggre_ep_rewards['min'], label="min")
@@ -117,6 +122,6 @@ env.close()
 import pickle
 
 filename = "q_table" + str(EPISODES) + ".obj"
-filehandler = open(filename,"wb")
-pickle.dump(q_table,filehandler)
+filehandler = open(filename, "wb")
+pickle.dump(q_table, filehandler)
 filehandler.close()
--- Q-Learning/test.py
+++ Q-Learning/test.py
@@ -1,32 +1,38 @@
-
 import pickle
 import gymnasium as gym
 import numpy as np
+
 # import matplotlib.pyplot as plt
 # from tqdm import tqdm
 import time
 
-file = open("q_table25000.obj",'rb')
+file = open("q_table25000.obj", "rb")
 q_table = pickle.load(file)
 file.close()
 
 # Show results of training
 
 new_env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = new_env.reset(seed = 50)
+observation, info = new_env.reset()
 
 
 DISCRETIZATION = 12
 
 DISCRETE_OBSERVATION_SPACE_SIZE = [DISCRETIZATION] * len(observation[:-2])
-discrete_os_win_size = (new_env.observation_space.high[:-2] - new_env.observation_space.low[:-2]) / DISCRETE_OBSERVATION_SPACE_SIZE
+discrete_os_win_size = (
+    new_env.observation_space.high[:-2] - new_env.observation_space.low[:-2]
+) / DISCRETE_OBSERVATION_SPACE_SIZE
+
 
 def get_discrete_state(state):
-    descrete_state = ((state[:-2] - new_env.observation_space.low[:-2]) / discrete_os_win_size)
-    descrete_state =  np.clip(descrete_state, 0 , DISCRETIZATION-1)
+    descrete_state = (
+        state[:-2] - new_env.observation_space.low[:-2]
+    ) / discrete_os_win_size
+    descrete_state = np.clip(descrete_state, 0, DISCRETIZATION - 1)
     # print(type(descrete_state))
     # print(tuple(descrete_state.astype(int).clip(0, DISCRETE_OBSERVATION_SPACE_SIZE-1)))
     return tuple(descrete_state.astype(int))
+
 
 terminated = False
 truncated = False
@@ -35,9 +41,9 @@ while not (terminated or truncated):
     # print(q_table[discrete_state])
     action = np.argmax(q_table[discrete_state])
     # print(action)
-    observation, reward, terminated, truncated, _ = new_env.step(action)    
-    new_discrete_state = get_discrete_state(observation)        
+    observation, reward, terminated, truncated, _ = new_env.step(action)
+    new_discrete_state = get_discrete_state(observation)
     discrete_state = new_discrete_state
-    time.sleep(0.1)
+    time.sleep(0.05)
     print(reward)
 new_env.close()
